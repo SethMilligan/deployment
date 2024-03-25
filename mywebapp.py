b49a80c5d6a288f9b30699d7229870c8fc7dbba2
Anna Prokofieva <annaprokofieva@techexchange.in>
2024-03-25 15:21:27 +0000

    Adding web app.

---
+++ mywebapp.py
@@ -0,0 +1,16 @@
+import streamlit as st 
+
+st.title("My Streamlit App")
+
+st.header("This is my containerized app")
+
+if 'buttonState' not in st.session_state:
+    st.session_state.buttonState = False
+
+def clickFn():
+    st.session_state.buttonState = True
+
+st.button("Click me!", on_click=clickFn)
+
+if st.session_state.buttonState:
+    st.write("You've clicked this button!")
